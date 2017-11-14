18229014588c2a5ddbae1ad52feccab7e9f03780
dmcBig <yu911512152@gmail.com>
2017-11-14 17:08:50 +0800

    Add files via upload

--- www/MediaPicker.js
+++ www/MediaPicker.js
@@ -4,6 +4,12 @@ var exec = require('cordova/exec');
 var MediaPicker = {
     getMedias:function(arg0, success, error) {
     	exec(success, error, "MediaPicker", "getMedias", [arg0]);
+	},
+	photoLibrary:function(arg0, success, error) {
+    	exec(success, error, "MediaPicker", "photoLibrary", [arg0]);
+	},
+	takePhoto:function(arg0, success, error) {
+    	exec(success, error, "MediaPicker", "takePhoto", [arg0]);
 	}
 };
 
